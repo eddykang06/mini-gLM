2ff01adeff107aed90ebb876ac066aa2bbb087ac
eddykang06 <eddykang06@gmail.com>
2026-06-15 09:07:37 -0400

    Moved final sampler to src

--- src/data.py
+++ src/data.py
@@ -74,30 +74,33 @@ def configure_root():
     return root
 
 
-# Replace later with efficient sample to write csv
-
 def sample_from_fasta(
-        fasta_file: str | Path,
-        chrom_size_file: str | Path,
+        data_dir: Path,
+        out_dir: Path,
         n_seqs: int,
         min_length: int,
         max_length: int,
         seed: int | None = None,
-) -> list:
+) -> pd.DataFrame:
     """
-    Function to sample random sequences of a specified length range from .fa file, weigthed by chromsome length
+    Function to sample random sequences of a specified length range from .fa file, weighted by chromsome length
+    Final output is written to csv.
 
     Args:
-        fasta_file      : Path to .fa file containing genome
-        chrom_size_file : Path to tab-separate text file containing sequence names and sizes
+        data_dir        : Path to dir containing data files
+        out_dir         : Path to write final csv to
         n_seqs          : Number of sequences to sample
         min_length      : Minimum sequence length to sample
         max_length      : Maximum sequence length to sample
         seed            : Random seed for numpy
     
     Returns:
-        seqs : List of sampled sequences as strings
+        csv : Dataframe of sampled sequences with start, end, length, and chromosome ID
     """
+    # Store paths to data
+    fasta_file = data_dir / "hg38.fa"
+    chrom_size_file = data_dir / "hg38.chrom.sizes"
+
     # Set random seed if specified
     rng = np.random.default_rng(seed)
 
@@ -120,8 +123,12 @@ def sample_from_fasta(
     # Normalize to obtain sampling weights for each chromosome
     weights = size_df["length"].to_numpy() / size_df["length"].to_numpy().sum()
 
-    # Initiate list to store sequences
+    # Initiate list to store sequences and metadata
     seqs = []
+    chroms = []
+    starts = []
+    ends = []
+    lengths = []
 
     # Counter so that rejected sequences don't count toward total count
     counter = 0
@@ -134,27 +141,38 @@ def sample_from_fasta(
         length = int(size_df.loc[chrom, "length"])
 
         # Randomly sample a sequence
-        start_idx = rng.integers(0, length)
-        seq_length = rng.integers(min_length, max_length + 1)
-
-        # If the sampled sequence is out of range
-        if start_idx + seq_length > length:
-            continue
-        
+        seq_length = rng.integers(min_length, min(length, max_length) + 1)
+        start_idx = rng.integers(0, length - seq_length + 1)
         seq = f[chrom][start_idx:start_idx + seq_length]
-        seq = str(seq)
+        seq = str(seq).upper()
 
         # Split across "N" character if exists and take first substr
         seq = seq.split("N")[0]
 
         # Convert to uppercase, remove non-alpha and non-ACGT characters 
-        seq = "".join(char.strip().upper() for char in seq if char.isalpha() and char in "ACGTacgt")
+        seq = "".join(char for char in seq if char in "ACGT")
+        new_length = len(seq)
 
         # Check that sequences is still above minimum length
-        if len(seq) >= min_length:
+        if new_length >= min_length:
             counter += 1 
+
+            # Append sequence and all metadata
             seqs.append(seq)
+            chroms.append(chrom)
+            starts.append(start_idx)
+            ends.append(start_idx + seq_length)
+            lengths.append(new_length)
 
-    seqs = np.array(seqs)
+    # Construct dataframe
+    df = pd.DataFrame({
+        "sequence": seqs,
+        "chromosome_id": chroms,
+        "start": starts,
+        "end": ends,
+        "length": lengths
+    })
 
-    return seqs
+    # Write to csv
+    out_file = out_dir / "pretraining.csv.gz"
+    df.to_csv(out_file, compression = "gzip", index = False)
